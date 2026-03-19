88a662d839016c61e95110c64ee3bf3dac3592f7
PJPRORL <piussi-jeroen@hotmail.com>
2026-03-19 20:07:07 +0100

    Updating scripts

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,10 @@
 
 // 1. Initialisatie: Bekijk de localStorage en laad de spellen
 // TIP: Gebruik de variabele 'initialGames' uit games.js als fallback!
-let activeGames = [initialGames];
+let activeGames = [ListOfGames];
 console.log(activeGames);
 
 // 2. Selecteer je HTML elementen (canvas, button, result div)
-
-
-// 3. Functie om het rad te tekenen op het canvas
-function drawWheel() {
-    // Jouw canvas code hier...
-
-}
-
-
-// 4. Functie om de animatie af te handelen
-function spinWheel() {
-    // Jouw animatie en logica code hier...
-
-}
 
 
 // 5. Functie om een spel permanent te verwijderen
