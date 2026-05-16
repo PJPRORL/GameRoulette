b3cbab337f4a9da92d842333e1c15f30a9f44bb4
Jeroen Piussi <r0615208@student.thomasmore.be>
2026-05-16 20:37:29 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: 'https://github.com/PJPRORL/GameRoulette', // Vervang dit door jouw repo-naam!
+    base: 'https://pjprorl.github.io/GameRoulette',
 })
